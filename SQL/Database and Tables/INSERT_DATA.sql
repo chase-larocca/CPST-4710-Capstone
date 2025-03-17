cad5040087aebd1acca7931a0359817615a19fa8
Chase LaRocca <39739602+chase-larocca@users.noreply.github.com>
2025-03-17 00:41:42 -0500

    Insert data to DB

---
+++ SQL/Database and Tables/INSERT_DATA.sql
@@ -0,0 +1,54 @@
+-- DATA INSERTS FOR SITE DATA POPULATION
+ SELECT * FROM ttops.inventory; 
+ SELECT * FROM ttops.inventorycolors; 
+
+-- INVENTORY DATA
+
+INSERT INTO Inventory (SKU, ItemName, ItemDescription, Price, QuantityInStock, Supplier, RestockThreshold, CreatedAt, UpdatedAt)
+VALUES
+('GOFIT-DT-PURPLE', 'GoFit Double Thick Yoga Mat', 'Extra-thick mat for comfort and support', 39.99, 50, 'GoFit', 10, NOW(), NOW()),
+('GOFIT-GREY', 'GoFit Yoga Mat', 'Standard durable GoFit yoga mat', 24.99, 50, 'GoFit', 10, NOW(), NOW()),
+('GOFIT-PATTERN', 'GoFit Pattern Yoga Mat w/ Yoga Pose Chart', 'Patterned mat with wall chart', 21.49, 50, 'GoFit', 10, NOW(), NOW()),
+('GOFIT-SUMMIT', 'GoFit Summit Yoga Mat', 'Premium yoga mat for professionals', 69.99, 50, 'GoFit', 10, NOW(), NOW()),
+('GOFIT-KIT', 'GoFit Yoga Kit: Mat, Block, Strap, Chart', 'All-in-one yoga kit for beginners', 29.99, 50, 'GoFit', 10, NOW(), NOW()),
+('GOFIT-DELUXE', 'GoFit Deluxe Pilates Foam Mat', 'Foam mat for yoga and pilates routines', 34.99, 50, 'GoFit', 10, NOW(), NOW());
+
+
+-- INVENTORY COLOR DATA 
+
+INSERT INTO Inventory (SKU, ItemName, ItemDescription, Price, QuantityInStock, Supplier, RestockThreshold, CreatedAt, UpdatedAt)
+VALUES 
+('BUB-40OZ-BLK', 'Bubba 40 oz. Radiant Stainless Steel Bottle', 'Rubberized stainless steel bottle with matte finish', 25.99, 100, 'Bubba Brands', 10, NOW(), NOW()),
+('BUB-HERO-BLK', 'Bubba Hero Mug', 'Travel mug with handle and vacuum insulation', 17.99, 100, 'Bubba Brands', 10, NOW(), NOW()),
+('BUB-32OZ-STRAW', 'Bubba 32 oz. Radiant Bottle w/ Straw', 'Rubberized stainless steel bottle with built-in straw', 22.99, 100, 'Bubba Brands', 10, NOW(), NOW()),
+('BUB-FLO-16OZ', 'Bubba Flo Refresh 16oz Bottle', 'Crystal Ice bottle with Rock Candy and Kiwi color wash', 12.99, 100, 'Bubba Brands', 10, NOW(), NOW()),
+('BUB-32OZ-LIC', 'Bubba 32 oz. Radiant Bottle - Licorice', 'Matte black stainless steel water bottle', 22.99, 100, 'Bubba Brands', 10, NOW(), NOW());
+
+-- INVENTORY COLOR DATA
+
+INSERT INTO InventoryColors (SKU, ColorName)
+VALUES 
+('BUB-40OZ-BLK', 'Black'),
+('BUB-40OZ-BLK', 'Navy'),
+('BUB-HERO-BLK', 'Black'),
+('BUB-32OZ-STRAW', 'Blue'),
+('BUB-FLO-16OZ', 'Green'),
+('BUB-FLO-16OZ', 'Blue-Green'),
+('BUB-32OZ-LIC', 'Licorice');
+
+
+
+INSERT INTO InventoryColors (SKU, ColorName)
+VALUES
+('GOFIT-DT-PURPLE', 'Purple'),
+('GOFIT-GREY', 'Grey'),
+('GOFIT-PATTERN', 'Pink'),
+('GOFIT-PATTERN', 'Green'),
+('GOFIT-PATTERN', 'Blue'),
+('GOFIT-SUMMIT', 'Purple'),
+('GOFIT-KIT', 'Blue'),
+('GOFIT-DELUXE', 'Blue');
+
+
+
+
